7453c038242651edc6a8716af4f97286f0429e5a
Turxan Dunyamaliyev <turxan.dunyamaliyev@kapitalbank.az>
2023-01-03 19:31:04 +0400

    minor refactorings

---
+++ src/main/basics/createthread/withRunnable/SimpleThreadWithRunnable.java
@@ -0,0 +1,31 @@
+package main.basics.createthread.withRunnable;
+
+class FirstRunner implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++)
+            System.out.println("FirstRunner: " + i);
+    }
+}
+
+class SecondRunner implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++)
+            System.out.println("SecondRunner: " + i);
+    }
+}
+
+public class SimpleThreadWithRunnable {
+    public static void main(String[] args) {
+        Thread firstThread = new Thread(new FirstRunner());
+        Thread secondThread = new Thread(new SecondRunner());
+
+        firstThread.start();
+        secondThread.start();
+
+        System.out.println("Main thread has ended");
+    }
+}
---
+++ src/main/basics/locks/lock/LockExample.java
@@ -0,0 +1,39 @@
+package main.basics.locks.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockExample {
+
+    private static int counter = 0;
+    private static final Lock lock = new ReentrantLock();
+
+    private static void increment() {
+        lock.lock();
+
+        try {
+            for (int i = 0; i < 100; i++) {
+                counter++;
+            }
+        } finally { //We have to use finally, in case of maybe exception happens while incrementing
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(LockExample::increment);
+        Thread t2 = new Thread(LockExample::increment);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(counter);
+    }
+}
